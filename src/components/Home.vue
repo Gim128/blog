dd023d39272dc3f0a33d66e3a593abe68485d285
ican-graphics-01 <lankaican@gmail.com>
2023-04-19 11:28:17 +0530

    how to use if conditions in vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1>Hello from Home</h1>
-    <button v-on:click="hello">Touch me</button>
+    <h1 v-if="show">Hello from Home</h1>
+    <h1 v-else>else side</h1>
+    <!-- <button v-on:click="hello">Touch me</button> -->
+    <button v-on:click="display">Toggle me!</button>
   </div>
 </template>
 
@@ -15,8 +17,17 @@ export default {
     methods:{
         hello(){
             alert("Hello function called!")
+        },
+        display(){
+            this.show=!this.show
         }
-    }
+    },
+    data(){
+        return {
+            show: false
+        }
+    },
+
 }
 </script>
 
