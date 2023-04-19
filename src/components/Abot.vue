5e85fea3c9d1ef862fe799e35e97649afa9933d8
ican-graphics-01 <lankaican@gmail.com>
2023-04-19 16:40:37 +0530

    thats the way how actually comp. works, routing

---
+++ src/components/Abot.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>About Us</h1>
+    <Users />
+    <products />
+  </div>
+</template>
+
+<script>
+
+import Users from './User.vue' 
+import Products from './Products.vue'
+
+export default {
+    
+    name: 'AboutComponent',
+    components: {
+        Users,
+        Products
+     },
+}
+</script>
+
+<style scoped>
+    .about{
+        background-color: blue;
+        height: 500px;
+        
+    }
+</style>
