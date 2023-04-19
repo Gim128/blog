e8c6f77b9fda5f7a7c280bc973b7ee24f4a5fd2d
ican-graphics-01 <lankaican@gmail.com>
2023-04-19 11:08:56 +0530

    fundamentals:crerate component & create click event

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello from Home</h1>
+    <button v-on:click="hello">Touch me</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeComponent',
+    props: {
+        data: String,
+        msg: String
+    },
+    methods:{
+        hello(){
+            alert("Hello function called!")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
