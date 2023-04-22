60c8f9289410d65258287260c169d592bfec0507
Gim128 <gimhanpabasara4@gmail.com>
2023-04-22 14:33:01 +0530

    child to parent data pass

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button v-on:click="UpdatedTitle">Update Header</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ChildComponent',
+
+        methods: {
+            UpdatedTitle(){
+                this.$emit('changeTitle', 'Props has been passed')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
-    <h1>Hello from Home</h1>
-    <button v-on:click="hello('data')">Touch me</button>
+    <h1>{{ title }}</h1>
+    <ChildComponent v-on:changeTitle="updateTitleText($event)" />
   </div>
 </template>
 
 <script>
+import ChildComponent from './ChildComponent.vue';
+
 export default {
     name: 'HomeComponent',
-    props: {
-        data: String,
-        msg: String
+   
+    components: {
+    ChildComponent
+},
+
+    data(){
+      return {
+        title: 'Child to Parent Data',
+      }
     },
-    methods:{
-        hello(item){
-            // alert("Hello function called!")
-            console.warn(item);
-        }
+
+    methods: {
+      updateTitleText(title) {
+        this.title = title
+      }
+
     }
 }
 </script>
