87e7db055fd09eabfbe7110de785f93edf781d75
Gim128 <gimhanpabasara4@gmail.com>
2023-04-22 18:57:45 +0530

    intro about life cycle methods(hooks)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,46 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <ChildComponent v-bind:users=users />
+    <ChildComponent />
   </div>
 </template>
 
 <script>
+
 import ChildComponent from './ChildComponent.vue';
 
 export default {
     name: 'HomeComponent',
+
+    methods:{
+      
+    },
+
+    data(){
+      return{
+        
+      }
+    },
+
+    beforeCreate:function(){
+      console.warn('beforeCreate');
+    },
+    created:function(){
+      console.warn('Created')
+    },
+    beforeMount:function(){
+      console.warn('beforeMount ')
+    },
+    mounted:function(){
+      console.warn('Mount ')
+    },
    
     components: {
     ChildComponent
 },
 
-    data(){
-      return {
-       users:[
-        {name: 'Gimhan Praba', email:'gimhan@gmail.com'},
-        {name: 'Santhush Praba', email:'santhusn@gmail.com'},
-        {name: 'Jeewan Praba', email:'jeewan@gmail.com'},
-        {name: 'Raveen Praba', email:'raveenn@gmail.com'},
-       ]
-      }
-    },
+    
 
-    methods: {
-      updateTitleText(title) {
-        this.title = title
-      }
-
-    }
+    
 }
 </script>
 
